f8369346e13f944248c9836f0231fb535be59409
Dima <dimafrontbackdev@gmail.com>
2023-01-29 15:44:39 +0200

    lines

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useRef, useState } from 'react';
+import kronshtein1 from './assets/kronshtein1.jpg';
 
 function App() {
+  const [f1, setF1] = useState('');
+  const [f2, setF2] = useState('');
+  const [fileName, setFileName] = useState('Upload your photo');
+  const [kronshteinImg, setKronshteinImg] = useState('');
+
+  const changeFile = (e) => {
+    if (!e.target.files) {
+      return;
+    }
+
+    setFileName(e.target.files[0]?.name ? e.target.files[0].name : 'Upload your photo');
+    setKronshteinImg(fileName);
+    console.log(fileName);
+  };
+
+  useEffect(() => {
+    console.log(f1);
+    console.log(f2);
+  }, [f1, f2]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="dano">
+        <div className="dano__row">
+          <div className="dano__column">
+            <p>Дано</p>
+            <p>
+              f1 = <input type="text" value={f1} onChange={(e) => setF1(e.target.value)} />
+            </p>
+            <p>
+              f2 = <input type="text" value={f2} onChange={(e) => setF2(e.target.value)} />
+            </p>
+          </div>
+          <div className="dano__column">
+            <img src={kronshteinImg ? kronshtein1 : ''} alt="" />
+            <input type="file" id="input__file" onInput={changeFile} />
+            <span>{fileName}</span>
+          </div>
+        </div>
+      </div>
+      <div className="build">
+        <div className="build__area">
+          <div className="build__line build__line--blue"></div>
+          {/* <div className="build__line build__line--green"></div> */}
+          {/* <div className="build__line build__line--red"></div> */}
+        </div>
+      </div>
     </div>
   );
 }
