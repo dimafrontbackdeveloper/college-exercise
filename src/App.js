203aa91dce3b8e5835da21258a821f1c93e5b058
Dima <dimafrontbackdev@gmail.com>
2023-01-31 15:30:05 +0200

    solution to example 1

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
   const [f2, setF2] = useState('');
   const [fileName, setFileName] = useState('Upload your photo');
   const [kronshteinImg, setKronshteinImg] = useState('');
+  const [example1, setExample1] = useState('');
+  const [rab1Hidden, setRab1Hidden] = useState(null);
 
   const changeFile = (e) => {
     if (!e.target.files) {
@@ -19,10 +21,68 @@ function App() {
     console.log(fileName);
   };
 
-  useEffect(() => {
-    console.log(f1);
-    console.log(f2);
-  }, [f1, f2]);
+  const solveExample1 = () => {
+    const example1LeftSideArr = [];
+    const example1RightSideArr = [];
+    let solutionToExample1 = [];
+    let isPushToLeftSide = true;
+
+    // const newExample1 = [];
+
+    example1.split(' ').forEach((el, i) => {
+      if (
+        el === 'Rab' ||
+        el === 'Rac' ||
+        el === '=' ||
+        (el === '-' && example1.split(' ')[i + 1] === 'Rac') ||
+        (el === '-' && example1.split(' ')[i + 1] === 'Rab') ||
+        (el === '*' && example1.split(' ')[i - 1] === 'Rac') ||
+        (el === '0' && example1.split(' ')[i - 1] === '=')
+      ) {
+        isPushToLeftSide = false;
+
+        if (el !== '=' && example1.split(' ')[i - 1] !== '=') {
+          solutionToExample1.push(el);
+        }
+
+        return;
+      }
+
+      if (isPushToLeftSide) {
+        if (el === 'F1') {
+          example1LeftSideArr.push(f1);
+        } else if (el === 'F2') {
+          example1LeftSideArr.push(f2);
+        } else if (el === 'cos30') {
+          example1LeftSideArr.push(0.867);
+        } else if (el === 'cos60') {
+          example1LeftSideArr.push(0.5);
+        } else {
+          example1LeftSideArr.push(el);
+        }
+      } else {
+        if (el === 'F1') {
+          example1RightSideArr.push(f1);
+        } else if (el === 'F2') {
+          example1RightSideArr.push(f2);
+        } else if (el === 'cos30') {
+          example1RightSideArr.push(0.867);
+        } else if (el === 'cos60') {
+          example1RightSideArr.push(0.5);
+        } else {
+          example1RightSideArr.push(el);
+        }
+      }
+    });
+
+    // convert from arr to solution to example
+    let example1LeftSide = eval(example1LeftSideArr.join(''));
+    let example1RightSide = eval(example1RightSideArr.join(''));
+    // console.log(eval(example1LeftSide));
+    // console.log(eval(example1RightSide));
+    solutionToExample1 = [example1LeftSide, ...solutionToExample1, example1RightSide];
+    console.log(eval(solutionToExample1));
+  };
 
   return (
     <div className="App">
@@ -36,6 +96,7 @@ function App() {
             <p>
               f2 = <input type="text" value={f2} onChange={(e) => setF2(e.target.value)} />
             </p>
+            <p>Rab - ?, Rac - ?</p>
           </div>
           <div className="dano__column">
             <img src={kronshteinImg ? kronshtein1 : ''} alt="" />
@@ -49,6 +110,19 @@ function App() {
           {/* <div className="build__line build__line--green build__line--green-gradus gradus-right-0"></div> */}
           <div className="build__line build__line--blue gradus-left-45"></div>
         </div>
+      </div>
+      <div className="examples">
+        <p>(1), ΣFx = 0</p>
+        <input
+          type="text"
+          placeholder="Складаємо рівняння рівноваги"
+          value={example1}
+          onChange={(e) => {
+            console.log(e.target.value);
+            setExample1(e.target.value);
+          }}
+        />
+        <button onClick={solveExample1}>Вирішити</button>
       </div>
     </div>
   );
